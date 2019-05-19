824b83f4b90e4dd4e9ea739962aee7bd43006ccf
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-19 19:41:18 +0530

    Create count_no.c

---
+++ count_no.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int n,count=0;
+    scanf("%d",&n);
+    while(n>0)
+   {
+      int r=n%10;
+      n=n/10;
+      count++;
+   }
+   printf("%d",count);
+    return 0;
+}
