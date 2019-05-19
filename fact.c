03b72af951facb55976692b69c2e0203c23c47c9
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-19 22:40:41 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,fact=1;
+    scanf("%d",&n);
+    if(n!=0)
+    {
+    for(int i=1;i<n;i++)
+    {
+        fact=fact+fact*i;
+    }
+    printf("%d",fact);
+    }
+    else
+    {
+        printf("1");
+    }
+
+    return 0;
+}
