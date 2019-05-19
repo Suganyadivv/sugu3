8d2cc31b5a717656b7b75501deec0bb84ba56b0e
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-19 22:06:25 +0530

    Create N_K.c

---
+++ N_K.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int size,k,sum=0;
+    scanf("%d",&size);
+    int n[size];
+    for(int i=1;i<=size;i++)
+    {
+       scanf("%d",&n[i]);  
+    }
+    scanf("%d",&k);
+   for(int i=1;i<=k;i++)
+   {
+       sum=sum+n[i];
+   }
+    printf("%d",sum);
+
+    return 0;
+}
