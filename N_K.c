208ea88284e3013b826ca9a584ddd33a36e8dd76
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-20 12:03:36 +0530

    Update N_K.c

--- N_K.c
+++ N_K.c
@@ -4,17 +4,16 @@ int main()
 {
     int size,k,sum=0;
     scanf("%d",&size);
+    scanf("%d",&k);
     int n[size];
     for(int i=1;i<=size;i++)
     {
        scanf("%d",&n[i]);  
     }
-    scanf("%d",&k);
    for(int i=1;i<=k;i++)
    {
        sum=sum+n[i];
    }
     printf("%d",sum);
-
     return 0;
 }
