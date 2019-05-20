ee9c2b30e1bf5168180b8749281778819352e2b9
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-20 12:12:32 +0530

    Update vow_cons.c

--- vow_cons.c
+++ vow_cons.c
@@ -10,7 +10,7 @@ int main()
     if((ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') || (ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'))  
     {
         
-        printf("Vowels");  
+        printf("Vowel");  
     }
     else if(ch>='a' && ch<='z'||ch>='A' && ch<='Z')
     {
