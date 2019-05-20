2e074b8ac677d859740167da27cbc00526258099
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-20 16:35:13 +0530

    Create arr_min.c

---
+++ arr_min.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int size,i,temp;
+    scanf("%d",&size);
+    int n[size];
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&n[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+    if(n[i]<n[i+1])
+    {
+         temp=n[i];
+         n[i]=n[i+1];
+         n[i+1]=temp;
+    }
+    }
+   printf("%d",temp);
+   return 0;
+}
