9fe22b12c4f8fdbd86dfe093f06231cd180dbe78
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-20 11:21:51 +0530

    Update num_checking.c

--- num_checking.c
+++ num_checking.c
@@ -6,16 +6,16 @@ int main()
     scanf("%d",&a);
     if(a>0)
     {
-        printf("positive");
+        printf("Positive");
     }
     else if(a<0)
     {
-        printf("negative");
+        printf("Negative");
         
     }
     else
     {
-        printf("zero");
+        printf("Zero");
     }
 
     return 0;
