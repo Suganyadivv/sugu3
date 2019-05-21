110cdd23bb7914e5e46c95075dcc820639bb1092
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-21 15:39:08 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int a=0,b=1,c,n,i;
+    int a=1,b=1,c,n,i;
     scanf("%d",&n);
-     printf("%d",b);
-    for(i=1;i<n;i++)
+    for(i=1;i<=n;i++)
     {
+        printf("%d",a);
         printf(" ");
         c=a+b;
         a=b;
         b=c;
-       printf("%d",c);
     }
   return 0;
 }
