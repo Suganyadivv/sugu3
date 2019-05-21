5c504df810cf6ecbbd108c1b92ada4f3bf7f1075
Suganyadivv <50793724+Suganyadivv@users.noreply.github.com>
2019-05-21 17:25:44 +0530

    Create lines_count.java

---
+++ lines_count.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String s1;
+		int count=0;
+		Scanner sc=new Scanner(System.in);
+		s1=sc.nextLine();
+		char b='.';
+	   char a[]=s1.toCharArray();
+	   for(int i=0;i<a.length;i++)
+	   { 
+	   if(a[i]==b)
+	   {
+	       count++;
+	   }
+	   }
+		System.out.println(count+1);
+		}
+}
